f4f1f91cd36d2cab4626b65fdb5412e731d07c21
Tompotio <atolenatsu@gmail.com>
2024-12-31 12:38:44 +0100

    ordonnancement fonctionnel pour case de base et serie parallèle

--- Partie_1/main.py
+++ Partie_1/main.py
@@ -74,14 +74,19 @@ print([node.label for node in forkTest.ordonnancementForkJoin()])
 # Tests pour Série parallèle
 
 # Création des bases
-base1 = SP_Base(2, 8, 4)
-base2 = SP_Base(5, 9, 3)
+base1 = SP_Base(1,1,"a","b",1)
+base2 = SP_Base(1,1,"c","d",1)
+base3 = SP_Base(1,1,"a1","b1",1)
+base4 = SP_Base(1,1,"c1","d1",1)
 
 # Composition en série
 serie = SP_Serie(base1, base2)
-ordonnancement = serie.ordonnancement()
+serie.print_SP()
+#serie2 = SP_Serie(base3, base4)
+#serie3 = SP_Serie(serie, serie2)
 
-print("Ordonnancement optimal :", [node.label for node in ordonnancement])
+#ordonnancement = serie3.ordonnancement()
+#print("Ordonnancement optimal :", [node.label for node in ordonnancement])
 
 '''
 # Composition en parallèle
--- Partie_1/parallel.py
+++ Partie_1/parallel.py
@@ -6,14 +6,22 @@ from forkjoin import *
 class SPGraph:
     '''
         Constructeur pour les séries parallèle.
+        Le constructeur gère deux cas. Les classes définies de manière récursive (en parallèle et en série) ou bien la classe de base.
+
         :param source : Poids du noeud source OU un noeud source.
         :param target : Poids du noud target OU un noeud target.
+        :param srcLabel : Nom du label source.
+        :param targetLabel : Nom du label target.
         :param edge_weight : Poids de l'arête entre les deux nœuds (facultatif).
     '''
-    def __init__(self, source, target, edge_weight = None):
-        if edge_weight is not None:
-            self.source = terminaisonNode(source, "s")
-            self.target = terminaisonNode(target, "t") 
+    def __init__(self, source, target, srcLabel="", targetLabel="", edge_weight=None):
+        srcLabel = srcLabel if srcLabel else "s"
+        targetLabel = targetLabel if targetLabel else "t"
+
+        # S'il y a le poids de l'arête. Et que source et target sont également des poids, alors on est en train de définir un case de base.
+        if (edge_weight is not None) and (isinstance(source, int)) and (isinstance(target, int)):
+            self.source = terminaisonNode(source, srcLabel)
+            self.target = terminaisonNode(target, targetLabel)
             self.edge_weight = edge_weight
         else:
             self.source = source
@@ -27,6 +35,7 @@ class SPGraph:
 
     '''
         Fusionne deux noeuds en faisant l'addition de leurs poids
+        Optionnellement on peut donner un nom au nouveau noeud (par défaut le nom est égal à la concaténation des labels des noeuds précédents)
     '''
     def fuseNode(self, node1, node2, label = ""):
         # Somme des noeuds
@@ -37,6 +46,7 @@ class SPGraph:
         if len(label) > 0: 
             newLabel = label
         else: 
+            print("Fusionne : ", node1.label, " avec ", node2.label)
             newLabel = node1.label + "/" + node2.label
         
         # Génère le nouveau noeud
@@ -50,10 +60,22 @@ class SPGraph:
 
     def plugTarget(self, newTarget, enfants):
         for enfant in enfants: 
-            enfant.parent["node"] = newTarget
+            enfant.parent[0] = newTarget
 
     def getType(self):
         return ""
+
+    def print_SP(self):
+        for parent in self.source.parent: 
+            current = self.source
+            output = "Affiche SP : [" + current.label
+
+            while current != self.target:
+                current = current.parent[0]
+                output += "," + current.label
+
+            output += "]"
+            print(output)
 
 class SP_Base(SPGraph):
     '''
@@ -62,14 +84,20 @@ class SP_Base(SPGraph):
         :param target : Noeud cible.
         :param weight : Poids de l'arête.
     '''
-    def __init__(self, sourceWeight, targetWeight, edge_weight):
-        super().__init__(sourceWeight, targetWeight, edge_weight)
+    def __init__(self, sourceWeight, targetWeight, srcLabel, targetLabel, edge_weight):
+        super().__init__(sourceWeight, targetWeight, srcLabel, targetLabel, edge_weight)
+
+        self.source.parent = [self.target]
+        self.target.enfant = [self.source]
 
     def ordonnancement(self):
         return []
 
     def getType(self):
         return "SP_Base"
+
+    def print_SP(self):
+        print("[", self.source.label, ",", self.target.label, "]")
 
 class SP_Serie(SPGraph):
     '''
@@ -84,8 +112,8 @@ class SP_Serie(SPGraph):
         fusedNode = self.fuseNode(G1.target, G2.source)
 
         # On plug le nouveau node avec les anciens parents et enfants
-        fusedNode.parent = G1.target.parent
-        fusedNode.enfant = G2.source.enfant
+        fusedNode.enfant = G1.target.enfant
+        fusedNode.parent = G2.source.parent
 
         # On plug les parents de la source de G2 au nouveau noeud
         self.plugSource(fusedNode, G2.source.parent)
@@ -100,16 +128,20 @@ class SP_Serie(SPGraph):
         self.G2 = G2
 
     def ordonnancement(self):
+        PremierMembre = [self.source]
+        SecondMembre = [self.G2.source]
+
+        # On récupère l'ordonnancement de G1 et on lui retire le premier et le denier élément
         G1_Ordo = self.G1.ordonnancement()
+        G1_Ordo = G1_Ordo[1:-1]
+
+        # On récupère l'ordo de G2 et on lui ajoute le self.target
         G2_Ordo = self.G2.ordonnancement()
+        G2_Ordo = G2_Ordo[:-1] # On retire le dernier élément de G2_Ordo par défaut
+        G2_Ordo.append(self.target)
 
-        # On retire le premier élément de G2_Ordo qui est commun avec G1_Ordo (dernier élément de celui-ci)
-        G2_Ordo_sans_premier = G2_Ordo[1:]
-        
-        G2_Ordo_sans_premier.append(self.target)
-
-        # Concatène les deux listes
-        return G1_Ordo + G2_Ordo_sans_premier
+        # Renvoie la concaténation du tout
+        return PremierMembre + G1_Ordo + SecondMembre + G2_Ordo 
 
     def getType(self):
         return "SP_Serie"
@@ -191,3 +223,21 @@ def lineariser(graphe : SPGraph, ordonnancement : list) :
 '''
 def ordonnancementSerieParallele(graphe : SPGraph):
     return graphe.ordonnancement()
+
+# Tests pour Série parallèle
+
+# Création des bases
+base1 = SP_Base(1,1,"a","b",1)
+base2 = SP_Base(1,1,"c","d",1)
+base3 = SP_Base(1,1,"a1","b1",1)
+base4 = SP_Base(1,1,"c1","d1",1)
+
+# Composition en série
+serie = SP_Serie(base1, base2)
+serie2 = SP_Serie(base3, base4)
+serie3 = SP_Serie(serie, serie2)
+
+ordonnancement = serie3.ordonnancement()
+
+#ordonnancement = serie3.ordonnancement()
+print("Ordonnancement optimal :", [node.label for node in ordonnancement])
